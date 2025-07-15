2c4392b05adef812d6ce413621324bd49bf4a6fa
RRKawchar <riyazurrohmankawchar@gmail.com>
2025-07-15 12:31:54 +0600

    initialize commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:blog_app/src/core/theme/app_theme.dart';
+import 'package:blog_app/src/features/auth/presentation/pages/sign_up_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Blog App',
+      theme: AppTheme.darkTheme,
+      home: SignUpPage()
+    );
+  }
+}
+
---
+++ lib/src/core/theme/app_theme.dart
@@ -0,0 +1,10 @@
+import 'package:blog_app/src/core/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme{
+
+
+  static final darkTheme=ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: AppColors.backgroundColor
+  );
+}
